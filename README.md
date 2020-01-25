db68eb0ad7219090398fff8f3b642a3c8f5ef038
Riko Sakurauchi <lijiahao99131@gmail.com>
2020-01-25 22:35:44 +0800

    allow beta coexist with release

--- README.md
+++ README.md
@@ -25,11 +25,9 @@ MTproto protocol manuals: https://core.telegram.org/mtproto
 You will require Android Studio 3.4, Android NDK rev. 20 and Android SDK 8.1
 
 1. Download the Nekogram source code from https://github.com/Nekogram/Nekogram ( git clone https://github.com/Nekogram/Nekogram.git )
-2. Copy your release.keystore into TMessagesProj/config
-3. Fill out RELEASE_KEY_PASSWORD, RELEASE_KEY_ALIAS, RELEASE_STORE_PASSWORD in local.properties to access your  release.keystore
-4. Open the project in the Studio (note that it should be opened, NOT imported).
-5. If you're compiling DEBUG version, make sure your build variants is set to afatDebugMultidex.
-6. You are ready to compile Nekogram.
+2. Open the project in the Studio (note that it should be opened, NOT imported).
+3. If you're compiling DEBUG version, make sure your build variants is set to afatDebugMultidex.
+4. You are ready to compile Nekogram. It takes time for the first-time build, be patient.
 
 ## Localization
 
--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -21,8 +21,8 @@ public class BuildVars {
     public static boolean TON_WALLET_STANDALONE = false;
     public static int BUILD_VERSION = 1849;
     public static String BUILD_VERSION_STRING = "5.14.0";
-    public static int APP_ID = 336779; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = "b91eefacc86747c068c8d8a16b41500d"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static int APP_ID = 1056817; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "31511ac2e6289df4bc7d33a880e99475"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
     public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
     public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
     public static String SMS_HASH = ""; //https://developers.google.com/identity/sms-retriever/overview
